ab1bece05361a542242a4ab55c5c08f6ccd0d9e3
Franz Andrei Cruspero <franz.cruspero@gmail.com>
2021-08-17 23:17:42 +0800

    Ran linter

--- frontend/tests/unit/addmodal.spec.js
+++ frontend/tests/unit/addmodal.spec.js
@@ -1,49 +1,46 @@
-import AddModal from "@/components/AddModal.vue"
-import { mount } from "@vue/test-utils"
-import axios from "axios"
+import AddModal from "@/components/AddModal.vue";
+import { mount } from "@vue/test-utils";
+import axios from "axios";
 
-jest.mock('axios', () => ({
-    post: jest.fn(() => {})
-}))
-
+jest.mock("axios", () => ({
+  post: jest.fn(() => {}),
+}));
 
 describe("addModal.vue", () => {
-    let wrapper = mount(AddModal, {
-        data() {
-            return {
-                type: "values",
-                title: "initial value",
-                description: "initial description"
-            }
-        }
-    })
+  let wrapper = mount(AddModal, {
+    data() {
+      return {
+        type: "values",
+        title: "initial value",
+        description: "initial description",
+      };
+    },
+  });
 
-    it("shows modal total", () => {
-        expect(wrapper.find("h5").text()).toBe("Add Value/Principle")
-    })
+  it("shows modal total", () => {
+    expect(wrapper.find("h5").text()).toBe("Add Value/Principle");
+  });
 
-    it("mocks axios post request and submit function", async() => {
-        const title = "Test title"
-        const description = "Test description"
+  it("mocks axios post request and submit function", async () => {
+    const title = "Test title";
+    const description = "Test description";
 
-        await wrapper.find("input").setValue(title)
-        await wrapper.find("textarea").setValue(description)
-        await wrapper.find('form').trigger('submit.prevent')
+    await wrapper.find("input").setValue(title);
+    await wrapper.find("textarea").setValue(description);
+    await wrapper.find("form").trigger("submit.prevent");
 
-        expect(axios.post).toHaveBeenCalledTimes(1)
+    expect(axios.post).toHaveBeenCalledTimes(1);
 
-        const titleField = await wrapper.get("input")
-        const descriptionField = await wrapper.get("textarea")
+    const titleField = await wrapper.get("input");
+    const descriptionField = await wrapper.get("textarea");
 
-        expect(titleField.text()).toBe("")
-        expect(descriptionField.text()).toBe("")
+    expect(titleField.text()).toBe("");
+    expect(descriptionField.text()).toBe("");
 
-        expect(wrapper.emitted()).toHaveProperty("submitForm")
-
-
-    })
-    it('renders correctly', () => {
-        const wrapper = mount(AddModal)
-        expect(wrapper.element).toMatchSnapshot()
-    })
-})
+    expect(wrapper.emitted()).toHaveProperty("submitForm");
+  });
+  it("renders correctly", () => {
+    const wrapper = mount(AddModal);
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
--- frontend/tests/unit/homepage.spec.js
+++ frontend/tests/unit/homepage.spec.js
@@ -1,16 +1,16 @@
 import HomePage from "@/components/HomePage.vue";
 import { shallowMount } from "@vue/test-utils";
 
-
 describe("homePage.vue", () => {
+  let wrapper = shallowMount(HomePage);
 
-    let wrapper = shallowMount(HomePage);
+  it("shows title header of the app", () => {
+    expect(wrapper.find("h1").text()).toBe(
+      "Values and Principles of Agile Software Development"
+    );
+  });
 
-    it("shows title header of the app", () => {
-        expect(wrapper.find("h1").text()).toBe("Values and Principles of Agile Software Development")
-    })
-
-    it('renders correctly', () => {
-        expect(wrapper.element).toMatchSnapshot()
-    })
+  it("renders correctly", () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
 });
